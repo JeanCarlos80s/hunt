1f2412cbd4f7ad2dd3a5c277711d489c380bb4ff
jeancarlos80s <jeancarlos80s@gmail.com>
2019-08-17 18:19:17 -0300

    first commit

---
+++ huntback/src/index.js
@@ -0,0 +1,12 @@
+// aula 04 - 18:50
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('./app/controllers/index')(app);
+
+app.listen(3000);
---
+++ huntweb/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Routes from './routes';
+
+import "./styles.css";
+
+import Header from "./components/header/index.";
+import Main from "./pages/main";
+
+const App = () => (
+  <div className="App">
+     <Header />
+     <Routes />
+  </div>
+);
+
+export default App;
